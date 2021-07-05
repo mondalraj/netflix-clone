b3ce990ffad9f8bfe206c853bad6e86c8d58aeee
Rajib Mondal <mondal021002rajib@gmail.com>
2021-07-05 22:27:27 +0530

    NavBar Component ready

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAeNNi4wXFZtyPrLMr2pgKbdTeLei1yGlo",
+    authDomain: "clone-projects-ec1aa.firebaseapp.com",
+    projectId: "clone-projects-ec1aa",
+    storageBucket: "clone-projects-ec1aa.appspot.com",
+    messagingSenderId: "163849021865",
+    appId: "1:163849021865:web:f227236be53c20ff717cb8"
+};
