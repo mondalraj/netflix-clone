c4e151000398702f907a36311a33e62734ff0f54
Rajib Mondal <mondal021002rajib@gmail.com>
2021-07-07 00:01:32 +0530

    TMDB API setup completed successfully

--- src/Banner.js
+++ src/Banner.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Banner.css';
+import axios from './axios';
+import requests from './Requests';
 
 function Banner() {
+
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[
+                Math.floor(Math.random() * request.data.results.length - 1)
+                ]
+            );
+            return request;
+        }
+        fetchData();
+    }, []);
+    console.log(movie);
+
     const truncate = (string, n) => {
         return string?.length > n ? string.substr(0, n - 1) + '...' : string;
     }
     return (
         <header className="banner" style={{
-            backgroundImage: `url("https://d107a8nc3g2c4h.cloudfront.net/images/blog/wp-content/uploads/Xclussive-US-Netflixbanner-bg.jpg")`,
+            backgroundImage: `linear-gradient(
+      90deg,
+      rgba(0, 0, 0, 1) 0%,
+      rgba(19, 19, 19, 0.7) 30%,
+      rgba(0, 0, 0, 0) 100%
+    ),url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
             backgroundPosition: "center center",
             backgroundSize: "cover",
             backgroundRepeat: "no-repeat"
@@ -15,18 +39,14 @@ function Banner() {
 
             <div className="banner__contents">
                 <h1 className="banner__title">
-                    Movie Name
+                    {movie?.title || movie?.name || movie?.original_name}
                 </h1>
                 <div className="banner__buttons">
                     <button className="banner__button">Play</button>
                     <button className="banner__button">My List</button>
                 </div>
                 <h3 className="banner__description">
-                    {truncate(`This is a test description This is a test descriptionThis is a test description This is a test description This is a test description
-                    This is a test description
-                    This is a test description
-                    This is a test description
-                    This is a test description This is a test description`, 150)}
+                    {truncate(movie?.overview, 150)}
 
                 </h3>
             </div>
---
+++ src/Requests.js
@@ -0,0 +1,12 @@
+const API_KEY = "f81980ff410e46f422d64ddf3a56dddd";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
+};
+
+export default requests;
